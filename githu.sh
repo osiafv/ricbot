f2c31b8d85bfaa9ee982e7326d7b9cc2e74a540c
Febrian Risky <skyzbott@gmail.com>
2024-06-17 13:27:11 +0800

    new

--- githu.sh
+++ githu.sh
@@ -1,17 +1,46 @@
+#token=$2
+#foldername=$3
+#commitmsg=$4
+
 foldername=$2
 commitmsg=$3
 
 pushrepo(){
-    git init 
-    git remote add origin git@github.com:osiafv/$foldername.git
+    git config --global --remove-section user
+    git config --global --remove-section credential
+    git remote remove origin
+    git config --global credential.helper store
+    git config --global user.user "osiafv"
+    git config --global user.email "skyzbott@gmail.com"
+    git config --global user.password "Risky132"
+    git config user.password "Risky132" 
+    git init
     git add .
     git commit -m "$commitmsg"
-    git push -u origin master
+    git branch -m main
+    git remote add origin https://github.com/osiafv/$foldername.git
+    git push -u origin main --force
 }
 
+pushrepo1(){
+    git config --global --remove-section user
+    git config --global --remove-section credential
+    git remote remove origin
+    git config --global user.user "osiafv"
+    git config --global user.email "skyzbott@gmail.com"
+    git config --global user.password "Risky132"
+    git config user.password "Risky132"
+    git config --global credential.helper store
+    git init
+    git add .
+    git commit -m "new" 
+    git branch -m main
+    git remote add origin https://github.com/osiafv/$foldername.git
+    git push -u origin main
+}
 update(){
     git add .
-    git commit -m "$foldername"
+    git commit -m "$commitmsg"
     git push
 }
 
@@ -19,7 +48,7 @@ update(){
 
 if [ $1 == "repo" ]
 then
-    node C:\gith.js $2 $3
+    #node C:\index.js $2 $3 $4 
 elif [ $1 == "pushrepo" ]
 then
     pushrepo
